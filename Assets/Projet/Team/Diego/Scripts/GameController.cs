a11aef89a4f76d6bf5509f0b5f3c6563cd48c80a
Unknown <francoisjanssens.pro@gmail.com>
2018-01-04 15:31:35 +0100

    Commit before Eloi Help

--- Assets/Projet/Team/Diego/Scripts/GameController.cs
+++ Assets/Projet/Team/Diego/Scripts/GameController.cs
@@ -8,9 +8,13 @@ public class GameController : MonoBehaviour
 
     #endregion
     #region Public Members
-    public int score;
+    public int score = 0;
+    public Player _player;
+    public EnumObject enumObj;
 
     
+
+
     #endregion
 
     #region Public void
@@ -21,17 +25,65 @@ public class GameController : MonoBehaviour
 
     void Start ()
     {
-        
+
     }
 
     void Update ()
     {
-        	
+        _BoucleDestruct();
     }
 
     #endregion
 
     #region Tools Debug and Utility
+
+    public void _BoucleDestruct()
+    {
+        if (_player.scoringObjectList.Count > 0)
+        {
+
+            int testboucle = 0;
+            foreach (GameObject _Object in _player.scoringObjectList)
+
+            {
+                EnumObject enumObj = _Object.GetComponent<EnumObject>();
+
+                if (enumObj.m_ScoringObjects.ToString() == "PacBall")
+                {
+
+                    Debug.Log("PacBall");
+                    score++;
+                }
+                if (enumObj.m_ScoringObjects.ToString() == "PacGum")
+                {
+
+                    Debug.Log("PacGum");
+                }
+                if (enumObj.m_ScoringObjects.ToString() == "Bonus")
+                {
+
+                    Debug.Log("Bonus");
+                }
+                if (enumObj.m_ScoringObjects.ToString() == "Enemy")
+                {
+
+                    Debug.Log("Enemy");
+                }
+
+                if (_player.scoringObjectList.Count == 1)
+                {
+                    Debug.Log("ok");
+                    score++;
+                    
+                }
+
+                testboucle++;
+
+            }
+            Debug.Log("Mon test boucle ="+ testboucle);
+            _player.DestroyScoring();
+        }
+    }
 
     #endregion
 
--- Assets/Projet/Team/Diego/Scripts/Player.cs
+++ Assets/Projet/Team/Diego/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     #region Public Members
     public List<GameObject> scoringObjectList;
     public int life;
+    
     #endregion
 
     #region Public void
@@ -27,23 +28,33 @@ public class Player : MonoBehaviour
     {
         	
     }
-    public void PickUp(GameObject obj) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("bonus"))
+        {
+            PickUp(collision.gameObject);
+        }
+    }
+
+
+    #endregion
+
+    #region Tools Debug and Utility
+    public void PickUp(GameObject obj)
+    {
         //tester la destruction d'objet
         scoringObjectList.Add(obj);
         obj.SetActive(false);
 
     }
-    public void DestroyScoring() {
+    public void DestroyScoring()
+    {
         foreach (GameObject obj in scoringObjectList)
         {
             Destroy(obj);
-        }      
+        }
     }
     #endregion
 
-    #region Tools Debug and Utility
 
-    #endregion
-
-    
 }
